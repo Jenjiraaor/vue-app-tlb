d8f7b53f0119bf1aa1ea94c632279dabc530c52f
jenjira <aoraoraor086@gmail.com>
2018-07-02 09:47:59 +0700

    updeat-4

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app right/>
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list>
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title><nuxt-link :to="item.url">{{ item.title }}</nuxt-link></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
     <v-toolbar app>
       <v-toolbar-title>VueApp</v-toolbar-title>
       <v-spacer/>
