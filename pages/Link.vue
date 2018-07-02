9e310caacf9cc53e8b36792586e45a7fd685b712
jenjira <aoraoraor086@gmail.com>
2018-07-02 09:06:05 +0700

    updeat-2

--- pages/Link.vue
+++ pages/Link.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <nuxt-link to="/table"><center>Hello jenjira->click.</center></nuxt-link><br>
-    <router-link :to="{ path: '/Login'}" replace><center>Register</center></router-link><br>
+    <nuxt-link to="/Login"><center>Hello jenjira->click.</center></nuxt-link><br>
+    <router-link :to="{ path: '/table'}" replace><center>Register</center></router-link><br>
     <router-link :to="{ path: '/Login'}" push><center>Register</center></router-link><br>
   </div>
 </template>
